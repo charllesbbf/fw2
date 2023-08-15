1d0c895fdc986763ffacd13de8bdb9cb432637d0
charllesbbf <133056435+charllesbbf@users.noreply.github.com>
2023-08-15 17:43:14 -0300

    Add files via upload

---
+++ Aulinha/script.js
@@ -0,0 +1,15 @@
+function validar (){
+    var nome = document.getElementById.getElementById("nome");
+
+
+
+
+if (nome.value==""){
+    alert ("campo vazio! preencha o nome");
+    nome.focus();
+    return;
+  }
+
+
+
+}
